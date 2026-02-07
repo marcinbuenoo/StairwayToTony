07e63b10c19f5643cf854971b7d0e3fbc98196a9
MarcioBuenoo <marcinbueno01@hotmail.com>
2026-02-06 23:38:09 -0300

    feat: make camera follow the player

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 @export var speed = 600
 
-func  _physics_process(delta):
+func  _physics_process(_delta):
 	var direction = Input.get_vector("move_left","move_right", 
 "move_up", "move_down")
 	velocity = direction * speed
